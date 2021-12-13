c1b3a6192bd7b5a7bb192bc10be51e56a18870d6
anujpandey0212 <MS20BTECH11003@iith.ac.in>
2021-12-13 13:10:51 +0530

    cards and side bar added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import SSidebar from './components/sidebar';
+import Ccards1 from './components/cards';
 
 function App() {
   return (
     <div className="App">
       <SSidebar/>
+      <Ccards1/>
     </div>
-  );
+  )
 }
 
 export default App;
--- my-app/src/components/sidebar/index.js
+++ my-app/src/components/sidebar/index.js
@@ -1,26 +1,45 @@
 import React from "react";
-import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { ProSidebar, Menu, MenuItem, SubMenu ,image} from 'react-pro-sidebar';
 import {  SidebarHeader, SidebarFooter, SidebarContent } from 'react-pro-sidebar';
 import {  FaGem,FaHeart} from "react-icons/fa";
 import 'react-pro-sidebar/dist/css/styles.css';
 import "./sidebar.css";
+import image1 from "../../assets/simphy-logo.png"
+import Hamburger from 'hamburger-react'
+import { useState } from 'react';
 
 export default function SSidebar(){
+     const [isOpen, setOpen] = useState(true);
+
+
 
     return (
         <div className="sidebar">
-            <ProSidebar>
-  <Menu iconShape="square">
-    <MenuItem icon={<FaGem />}>Dashboard</MenuItem>
-    <SubMenu title="Components" icon={<FaHeart />}>
-      <MenuItem>Component 1</MenuItem>
-      <MenuItem>Component 2</MenuItem>
-    </SubMenu>
-  </Menu>
-  <SidebarFooter icon={<FaHeart/>}>
-    github
-  </SidebarFooter>
-</ProSidebar>;
+            <ProSidebar collapsed={isOpen} collapsedWidth={70}>
+                <SidebarHeader className="header1">
+                    <Hamburger onToggle={toggled => {
+                            if (toggled) {
+                                setOpen(false);
+                            } else {
+                                setOpen(true);
+                            }
+                            }} />
+                            {/* <img src="image1" className="image1"></img> */}
+                </SidebarHeader>
+                <SidebarContent>
+                    <Menu iconShape="square">
+                        <MenuItem icon={<FaGem />}>Dashboard</MenuItem>
+                        <SubMenu title="Components" icon={<FaHeart />}>
+                            <MenuItem>Component 1</MenuItem>
+                            <MenuItem>Component 2</MenuItem>
+                        </SubMenu>
+                    </Menu>
+                </SidebarContent>
+                <SidebarFooter icon={<FaHeart/>}>
+                    github
+                </SidebarFooter>
+  
+            </ProSidebar>
         </div>
     )
 
