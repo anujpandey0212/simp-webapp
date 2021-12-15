d045264dda2b457928230f56f95cd50e646d7c9c
anujpandey0212 <MS20BTECH11003@iith.ac.in>
2021-12-15 15:05:36 +0530

    lightbox added

--- client/src/components/cards/index.js
+++ client/src/components/cards/index.js
@@ -6,8 +6,7 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import "./cards.css";
-import image2 from "../../assets/test.png"
-
+import SkyLight from 'react-skylight';
 
 
 export default function BasicCard() {
@@ -22,24 +21,54 @@ export default function BasicCard() {
   });
   }, []);
 
+  var myBigGreenDialog = {
+    backgroundColor: '#00897B',
+    color: '#ffffff',
+    width: '70%',
+    height: '600px',
+    marginTop: '-300px',
+    marginLeft: '-35%',
+  };
+ 
   return (
       <div className='cards1' id='cards1'>
         {data?.map((data, k) => (
-                    <Card sx={{ minWidth: 275 }}>
+                    <Card sx={{ minWidth: 275 }} key={k}>
                     <CardContent>
-                     <img src={!data?"loading":data.image} height={110} className='image2'></img>
+                     <img src={data.image} height={110} className='image2'></img>
                      <Typography >
-                        Category - {!data?"loading":data.name}
+                        Category - {data.name}
                       </Typography>
                       <Typography>
-                        Topic Name - {!data?"loading":data.topic_name}
+                        Topic Name - {data.topic_name}
                       </Typography>
                     </CardContent>
                     <CardActions>
-                      <Button size="small">Learn More</Button>
+                      <Button size="small" onClick={()=>{React.Component.customDialog.show()
+                     var frame1= document.getElementById("frame1");
+                     frame1.src=data.src;
+                      }}>Learn More</Button>
                     </CardActions>
+                    
                   </Card>
-                ))}
+                  
+                ))
+                }
+                <div>
+                  <SkyLight dialogStyles={myBigGreenDialog} hideOnOverlayClicked ref={ref => React.Component.customDialog = ref} title="A Custom Modal">
+                      I'm a custom modal!
+                      <iframe src="https://www.simphy.com" id='frame1' height={500} ></iframe>
+                  </SkyLight>
+                  </div>
+                
+
     </div>
+   
   );
 }
+
+// function customElements(){
+// return(
+  
+// )
+// }
